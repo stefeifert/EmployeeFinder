c13bc3decf2b70de220446631c2fced49bbbc753
Shiny <stefanie.eifert@yahoo.com>
2019-02-11 09:38:12 -0500

    added package.json file

--- server.js
+++ server.js
@@ -3,39 +3,40 @@ const path = require('path');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.port || 3000;
 
-app.get('/', function(request, response) {
-    response.sendFile(path.join(__dirname, 'public/home.html'));
-  });
-  
-app.get('/', function(request, response) {
-    response.sendFile(path.join(__dirname, 'public/survey.html'));
-  });
-  
+app.get('/', function (request, response) {
+  response.sendFile(path.join(__dirname, 'public/home.html'));
+});
+
+app.get('/', function (request, response) {
+  response.sendFile(path.join(__dirname, 'public/survey.html'));
+});
+
+
+
+app.get('/api/employees', function (req, res) {
+  return res.json(employees);
+});
+
+app.get('/api/employees/:employee', function (req, res) {
+  const chosen = req.params.employee;
+  console.log(chosen);
+  for (let i = 0; i < employees.length; i++) {
+    if (chosen === employees[i].routeName) {
+      return res.json(employees[i]);
+    }
+  }
+
+  return res.send('No employee found');
+});
+
 //WHERE DOES THIS GO??//
-  app.listen(PORT, function() {
-    console.log(`Server is listening on: http://localhost:${PORT}`);
-  });
+app.listen(PORT, function () {
+  console.log(`Server is listening on:${PORT}`);
+});
   //*
 
-app.get('/api/employees', function(req, res) {
-    return res.json(employees);
-  });
-
-app.get('/api/employees/:employee', function(req, res) {
-    const chosen = req.params.employee;
-    console.log(chosen);
-    for (let i = 0; i < employees.length; i++) {
-      if (chosen === employees[i].routeName) {
-        return res.json(employees[i]);
-      }
-    }
-  
-    return res.send('No employee found');
-  });
-  
 
 
-  
-  
+
