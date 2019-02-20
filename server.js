2047e8b9411c03ba5d65075238e7ac8abb4e18b4
Shiny <stefanie.eifert@yahoo.com>
2019-02-20 14:57:19 -0500

    we added the types of files being sent on our style.css links. We re-organized, and deleted the second var app=express() declaration. We removed extra header html tags for boostrap and other extraneous things

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
+
 // Node Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
-var app = express();
-
-app.use(express.static(path.join(__dirname, './app/public')));
-
-// Link in html and api routes
-var apiRoutes = require('./app/routing/apiRoutes.js');
-var htmlRoutes = require('./app/routing/htmlRoutes.js');
-
 // Set up Express App
 var app = express();
 var PORT = process.env.PORT || 3000;
+
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+app.use(express.static(path.join(__dirname, "./app/public")));
+
+// Link in html and api routes
+var apiRoutes = require('./app/routing/apiRoutes.js');
+var htmlRoutes = require('./app/routing/htmlRoutes.js');
+
 
 // Server Routing Map 
 apiRoutes(app); // API route - Must be listed first due to the HTML default catch all "use" route
